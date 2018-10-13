d06a1a098cb948b381edfcf7a6ec4f1cbca40051
Christina Kim <christinahyunakim@gmail.com>
2018-10-13 07:07:50 -0500

    initial commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+(function($) {
+    "use strict"; // Start of use strict
+
+$('.navbar').on('click', 'a[href^="#"]', function (event) {
+
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 500);
+});
+
+$('.portfolio-modal').on('show.bs.modal', function(e) {
+    $(".navbar").addClass("d-none");
+  })
+  $('.portfolio-modal').on('hidden.bs.modal', function(e) {
+    $(".navbar").removeClass("d-none");
+  })
+
+})(jQuery);
